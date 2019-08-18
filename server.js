3ec202acd62eebc17e4a6a80aa410247d0a082a0
Glitch (roan-almanac) <none>
2019-08-18 18:44:40 +0000

    🏭🏓 Checkpoint ./server.js:6026745/1036

--- server.js
+++ server.js
@@ -24,6 +24,28 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+/**
+ * A date string is valid if can be successfully parsed by new Date(date_string) (JS) . 
+ * Note that the unix timestamp needs to be an integer (not a string) specifying milliseconds.
+ * In our test we will use date strings compliant with ISO-8601 (e.g. "2016-11-20") because this 
+ * will ensure an UTC timestamp.
+ * If the date string is empty it should be equivalent to trigger new Date(), 
+ *  i.e. the service uses the current timestamp.
+ * If the date string is valid the api returns a JSON having the structure 
+ *  {"unix": <date.getTime()>, "utc" : <date.toUTCString()> } 
+ *  e.g. {"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}.
+ * If the date string is invalid the api returns a JSON having the structure 
+ *  {"unix": null, "utc" : "Invalid Date" }.
+ * It is what you get from the date manipulation functions used above.
+ */
+const isValidTime = (timeStr) => {};
+
+app.route('/api/timestamp/:date_string?')
+  .get((req, res) => {
+    res.json()
+  })
+  ;
+
 
 
 // listen for requests :)
