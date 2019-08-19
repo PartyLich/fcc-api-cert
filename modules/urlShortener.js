c9fb8ab2962822acd30a4c3cd66f09e5209afcd3
Glitch (roan-almanac) <none>
2019-08-19 18:13:45 +0000

    👧👮 Checkpoint ./server.js:6026745/418 ./modules/urlShortener.js:6026745/372

--- modules/urlShortener.js
+++ modules/urlShortener.js
@@ -27,21 +27,27 @@ mongoose.connect(process.env.MONGO_URI);
 /** validate long url provided by user
  */
 const validateUrl = (req, res, next) => {
-  const longUrl = req.query.;
+  const longUrl = req.body.url;
+  
+  req.invalid = false;
+  
   next();
 };
 
 /**
  */
 const createShortUrl = (req, res, next) => {
+  if(req.invalid) return next();
+  
   ShortUrl.find();
   
-  let original_url = 'www.google.com';
+  // const original_url = req.body.url;
+  const longUrl = 'www.google.com';
   let short_url = '1';
   
   
   req.shortUrl = {
-    original_url,
+    original_url: longUrl,
     short_url,
   };
   next();
@@ -50,6 +56,9 @@ const createShortUrl = (req, res, next) => {
 /** send json response
  */ 
 const sendShortUrl = (req, res) => {
+  const invalidUrl = {"error":"invalid URL"};
+  
+  if(req.invalid) res.json(invalidUrl);
   // const shortUrl = req.shortUrl;
   const shortUrl = {"original_url":"www.google.com","short_url":1};
   res.json(shortUrl);
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static('public'));
 
 // body parser middleware
-app.use(bodyParser.urlEncoded());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -34,18 +34,14 @@ app.get("/api/hello", function (req, res) {
 
 /** Timestamp microservice
  * /api/timestamp/:date_string?
- * A date string is valid if can be successfully parsed by new Date(date_string) (JS) . 
- * Note that the unix timestamp needs to be an integer (not a string) specifying milliseconds.
- * In our test we will use date strings compliant with ISO-8601 (e.g. "2016-11-20") because this 
- * will ensure an UTC timestamp.
+ * 
  * If the date string is empty it should be equivalent to trigger new Date(), 
  *  i.e. the service uses the current timestamp.
  * If the date string is valid the api returns a JSON having the structure 
  *  {"unix": <date.getTime()>, "utc" : <date.toUTCString()> } 
  *  e.g. {"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}.
  * If the date string is invalid the api returns a JSON having the structure 
- *  {"unix": null, "utc" : "Invalid Date" }.
- * It is what you get from the date manipulation functions used above.
+ *  {"unix": null, "utc" : "Invalid Date" }
  */
 const {parseDate, serveTimestamp} = require('./modules/timeStamp') 
 
