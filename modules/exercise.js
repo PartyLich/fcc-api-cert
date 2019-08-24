ae48606166b5a1475150f76be53e19ec527e7298
Glitch (fcc-timestamp-microservice-x) <none>
2019-08-24 00:09:12 +0000

    👴🌄 Checkpoint ./server.js:6026745/136 ./modules/exercise.js:6026745/762

---
+++ modules/exercise.js
@@ -0,0 +1,19 @@
+// POST /api/exercise/add
+  // lookup user
+    // res.status(400).json({error: 'unknown _id'});
+
+// save exercise
+
+// send response
+
+
+// POST /api/exercise/new-user
+  // valid input
+    // res.status(400).json({error: 'Path `username` is required.'});
+
+  // add user
+
+  // send response
+
+// GET /api/exercise/log?{userId}[&from][&to][&limit]
+// const getExercise
