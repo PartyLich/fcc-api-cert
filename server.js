bbd971a81bcda4fbbc4cc526e285e6366aafd056
Arthur E. Jones <PartyLich@gmail.com>
2019-08-24 15:26:03 -0400

    style: prefer const

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const app = express();
 const bodyParser = require('body-parser');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 // so that your API is remotely testable by FCC
+const cors = require('cors');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
